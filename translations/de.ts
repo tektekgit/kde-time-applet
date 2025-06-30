236dff032ff0e4ddeff9f4e77d4d905847621e8b
tektekgit <81643006+tektekgit@users.noreply.github.com>
2025-06-30 18:31:03 +0300

    Create de.ts

---
+++ translations/de.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+    <context>
+        <name>time_and_date</name>
+        <message>
+            <source>Time: %1</source>
+            <translation>Zeit: %1</translation>
+        </message>
+    </context>
+</TS>
